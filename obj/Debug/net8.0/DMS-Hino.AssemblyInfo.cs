bed1de757bebdc03b7b08bbc1a3fd3ed490bc7ea
Trainee ITD1 (HMMI) <trainee.itd1@hino-motors.co.id>
2024-10-25 13:25:47 +0700

    coba commit

--- obj/Debug/net8.0/DMS-Hino.AssemblyInfo.cs
+++ obj/Debug/net8.0/DMS-Hino.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DMS-Hino")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e891f7fafa1b01c694f7dab60b7fbbb6f27327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcdec4157d89e1fd7b17a4a472b3d95c44a5cd26")]
 [assembly: System.Reflection.AssemblyProductAttribute("DMS-Hino")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DMS-Hino")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
